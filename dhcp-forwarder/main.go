1741df0340ab171aebd19040d9753e2a2a1b56e1
Derek Wuelfrath <dwuelfrath@inverse.ca>
2017-04-13 13:24:51 -0400

    Remove Windows eventviewer dependancy for logging to allow Linux support Complete rewrite of the file due to gofmt

--- dhcp-forwarder/main.go
+++ dhcp-forwarder/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"io/ioutil"
+	"net"
+	"os"
+
 	"github.com/google/gopacket"
 	_ "github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
+	"github.com/google/logger"
 	"github.com/spf13/viper"
-	"golang.org/x/sys/windows/svc/eventlog"
-	"net"
-	"os"
 )
 
 var (
@@ -20,25 +22,14 @@ var (
 	snaplen int32 = 1600
 	host    string
 	port    string
-
-	Logger *eventlog.Log
 )
 
 func main() {
 	//Setup logging
 	const name = "DHCP-Forwarder"
-	const supports = eventlog.Error | eventlog.Warning | eventlog.Info
-	eventlog.Remove(name)
-	err := eventlog.InstallAsEventCreate(name, supports)
-	checkError(err)
-	defer func() {
-		err = eventlog.Remove(name)
-		checkError(err)
-	}()
 
-	Logger, err = eventlog.Open(name)
-	checkError(err)
-	defer Logger.Close()
+	// Logger setup
+	logger.Init(name, false, true, ioutil.Discard)
 
 	viper.SetConfigName("DHCP-Forwarder") // will match DHCP-Forwarder.{toml,json} etc.
 
@@ -68,10 +59,10 @@ func main() {
 	err = handle.SetBPFFilter(filter + exclude)
 	checkError(err)
 
-	Logger.Info(1, os.Args[0]+" started")
-	Logger.Info(1, "BPF set to: "+filter+exclude)
-	Logger.Info(1, "Listening on device: "+dev)
-	Logger.Info(1, "Forwarding packets to: "+host+" on udp port "+port)
+	logger.Info(1, os.Args[0]+" started")
+	logger.Info(1, "BPF set to: "+filter+exclude)
+	logger.Info(1, "Listening on device: "+dev)
+	logger.Info(1, "Forwarding packets to: "+host+" on udp port "+port)
 
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
@@ -87,13 +78,13 @@ func handlePacket(p gopacket.Packet) {
 		// The endpoint might not be listening yet.
 	}
 	if err := p.ErrorLayer(); err != nil {
-		Logger.Warning(2, "Error decoding some part of the packet.")
+		logger.Info(2, "Error decoding some part of the packet.")
 	}
 }
 
 func checkError(err error) {
 	if err != nil {
-		Logger.Error(3, err.Error())
+		logger.Error(3, err.Error())
 		panic(err.Error())
 	}
 }
